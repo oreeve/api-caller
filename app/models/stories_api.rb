f928f71d41e8a08e1c18b1d0f09a51293d074f1d
Olivia Reeve <olivia@greenfieldhq.com>
2016-06-09 15:47:45 -0400

    added comments

--- app/models/stories_api.rb
+++ app/models/stories_api.rb
@@ -6,6 +6,7 @@ class StoriesApi
   def initialize
   end
 
+  # Loop to call the api every 60 seconds
   def call
     while true do
       begin
@@ -17,6 +18,25 @@ class StoriesApi
     end
   end
 
+  # Gets new stories from the API since now - 2 minutes (1 minute overlap)
+  def get_new_stories
+    base_url = "https://www.informagm.com/"
+    time = Time.now.utc-2.minutes
+    path = "/api/v1/feeds/stories?updated_at=#{time.strftime("%FT%T")+"%2B00%3A00"}&per=50"
+
+    conn = Faraday.new(url: base_url) do |faraday|
+      faraday.headers[:Accept] = 'application/json'
+      faraday.headers[:Authorization] = "Token token=903890bfe6f5dcbb231e472c0ee33ed7, company_key=61047dce-b69a-4de1-986e-e6db9d46ef97"
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter :net_http
+    end
+
+    response = conn.get(path)
+    JSON.parse(response.body)
+  end
+
+  # Saves the stories to the db from #get_new_stories
   def save_stories
     Rails.logger.info("Stories API called at #{Time.now}")
     data = get_new_stories
@@ -37,29 +57,13 @@ class StoriesApi
     end
   end
 
-  def get_new_stories
-    base_url = "https://www.informagm.com/"
-    time = Time.now.utc-2.minutes
-    path = "/api/v1/feeds/stories?updated_at=#{time.strftime("%FT%T")+"%2B00%3A00"}&per=50"
-
-    conn = Faraday.new(url: base_url) do |faraday|
-      faraday.headers[:Accept] = 'application/json'
-      faraday.headers[:Authorization] = "Token token=903890bfe6f5dcbb231e472c0ee33ed7, company_key=61047dce-b69a-4de1-986e-e6db9d46ef97"
-      faraday.request :url_encoded
-      faraday.response :logger
-      faraday.adapter :net_http
-    end
-
-    response = conn.get(path)
-    JSON.parse(response.body)
-  end
-
+  # Gets all stories from the time of the first call in the loop (set manually)
   def get_all_stories
     base_url = "https://www.informagm.com/"
     time = "2016-06-09T14%3A52%3A41%2B00%3A00&per=150"
-    per = SavedStory.all.count
-    # path = "/api/v1/feeds/stories?per=#{per}"
     path = "/api/v1/feeds/stories?updated_at=#{time}"
+    # per = SavedStory.all.count
+    # path = "/api/v1/feeds/stories?per=#{per}"
 
     conn = Faraday.new(url: base_url) do |faraday|
       faraday.headers[:Accept] = 'application/json'
@@ -78,6 +82,7 @@ class StoriesApi
     return all_ids
   end
 
+  # Compares the stories from #get_all_stories to those saved in the db
   def compare_stories
     saved = SavedStory.all.map(&:story_id)
     all = get_all_stories
@@ -87,6 +92,7 @@ class StoriesApi
   end
 
 ### Development Methods
+# Same as above but points to localhost instead of informagm
 
   def get_new_dev_stories
     base_url = "http://localhost:3000/"
@@ -128,9 +134,9 @@ class StoriesApi
   def get_all_dev_stories
     base_url = "http://localhost:3000/"
     time = "2016-06-09T18%3A01%3A50%2B00%3A00&per=150"
-    per = SavedStory.all.count
-    # path = "/api/v1/feeds/stories?per=#{per}"
     path = "/api/v1/feeds/stories?updated_at=#{time}"
+    # per = SavedStory.all.count
+    # path = "/api/v1/feeds/stories?per=#{per}"
 
     conn = Faraday.new(url: base_url) do |faraday|
       faraday.headers[:Accept] = 'application/json'
@@ -143,7 +149,6 @@ class StoriesApi
     response = conn.get(path)
     data = JSON.parse(response.body)
     stories = data["stories"]
-    # binding.pry
     all_ids = []
     stories.each { |story| all_ids << story["id"] }
     return all_ids
@@ -163,7 +168,6 @@ class StoriesApi
   def compare_dev_stories
     saved = SavedStory.all.map(&:story_id)
     all = get_all_dev_stories
-    # binding.pry
     puts saved.count.to_s + " saved and " + all.count.to_s + " total. Same IDs?"
     saved.sort == all.sort
   end
